1a6e5c5ffbe0ce87500331d144103b3833f3f119
CangYu <yhcy1993@gmail.com>
2017-04-08 00:37:54 +0800

    cross lines generated

--- src/main.py
+++ src/main.py
@@ -28,18 +28,24 @@ for i in range(0, len(z)):
 front_crv = Curve(front_pts)
 tail_crv = Curve(tail_pts)
 
+"""
+# 前缘线
 a, b, c = front_crv.generate()
 plane.AddPart(IGES_Entity126(front_crv.p, front_crv.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
 
+# 尾缘线
 a, b, c = tail_crv.generate()
 plane.AddPart(IGES_Entity126(tail_crv.p, tail_crv.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
 
+# 前后缘采样点
 for i in range(0, len(z)):
     plane.AddPart(IGES_Entity116(x_front[i], y_front[i], z[i]))
     plane.AddPart(IGES_Entity116(x_tail[i], y_tail[i], z[i]))
 
+# 根梢弦线
 plane.AddPart(IGES_Entity110([x_front[0], y_front[0], z[0]], [x_tail[0], y_tail[0], z[0]]))
 plane.AddPart(IGES_Entity110([x_front[len(z) - 1], y_front[len(z) - 1], z[len(z) - 1]], [x_tail[len(z) - 1], y_tail[len(z) - 1], z[len(z) - 1]]))
+"""
 
 profile_pts = []
 profile_crv = []
@@ -52,10 +58,26 @@ for i in range(0, len(z)):
     profile_pts.append(pts)
     profile_crv.append(cc)
 
+    """
+    # 翼型离散点
     for k in range(0, len(pts)):
         plane.AddPart(IGES_Entity116(pts[k][0], pts[k][1], pts[k][2]))
+    """
 
+    # 翼型NURBS曲线，默认5阶
     a, b, c = cc.generate()
     plane.AddPart(IGES_Entity126(cc.p, cc.n, 1, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 1.0], float)))
+
+n = len(profile_pts[0])
+m = len(z)
+
+for i in range(0, n):
+    pts = np.zeros((m, 3), float)
+    for j in range(0, m):
+        pts[j] = profile_pts[j][i]
+
+    cc = Curve(pts)
+    a, b, c = cc.generate()
+    plane.AddPart(IGES_Entity126(cc.p, cc.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
 
 plane.Generate()
