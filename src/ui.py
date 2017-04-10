3afac88ef0c47eaee93622114c6f52e26ca7368c
CangYu <yhcy1993@gmail.com>
2017-04-10 22:30:11 +0800

    add ui frame

--- src/ui.py
+++ src/ui.py
@@ -1,1 +1,39 @@
-from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+import sys
+import os
+import math
+
+# 翼型文件夹路径
+airfoil_dir = '../airfoil/'
+
+# 翼型列表
+airfoil_list = []
+
+
+def update_airfoil_list():
+    for f in os.listdir(airfoil_dir):
+        cur_filename = os.path.join(airfoil_dir, f)
+        if os.path.isfile(cur_filename) and cur_filename.endswith('.dat'):
+            airfoil_list.append(f)
+
+
+class AircraftUI(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+    def initUI(self):
+        self.setWindowTitle('BWB一体化设计')
+        self.setWindowIcon(QIcon('../pic/plane_icon.jpg'))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    update_airfoil_list()
+
+    pm = AircraftUI()
+    pm.initUI()
+    pm.show()
+
+    sys.exit(app.exec_())
