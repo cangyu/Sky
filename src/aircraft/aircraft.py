8c09d72ff4799ca41a170bad2d37653249229dc1
CangYu <yhcy1993@gmail.com>
2017-04-11 10:49:56 +0800

    primary ui added, to isolate aircraft utilities

---
+++ src/aircraft/aircraft.py
@@ -0,0 +1,85 @@
+from src.iges.iges_entity110 import *
+from src.iges.iges_entity116 import *
+from src.iges.iges_entity126 import *
+from src.aircraft.wing import *
+from src.nurbs.curve import *
+
+plane = IGES_Model()
+
+z = np.array([0., 0.624029, 1.38967, 2.43503, 3.73439, 5.25574, 6.96162, 8.81003, 10.7555, 12.75, 14.7445, 16.69, 18.5384, 20.2443, 21.7656, 23.065, 24.1103, 24.876, 25.343, 25.5])
+x_front = np.array([0., 0.05, 0.3, 1.7, 4.9, 6.85, 8.45, 9.65, 10.6, 11.1, 11.7, 12.1, 12.4, 12.8, 13.2, 13.7, 14.1, 14.5, 15.2, 16.])
+y_front = np.linspace(0, 0, len(z))
+x_tail = np.array([19.7, 19.6, 19.6, 19.5, 19.3, 19, 18.3, 17.3, 16.6, 16.5, 16.8, 17, 17.45, 17.8, 18.1, 18.4, 18.55, 18.65, 18.3, 17.8])
+y_tail = np.linspace(0, 0, len(z))
+thickness = np.ones(len(z))
+airfoil = []
+for i in range(0, len(z)):
+    airfoil.append(Airfoil("../airfoil/naca0012.dat"))
+
+front_pts = np.zeros((len(z), 3), float)
+tail_pts = np.zeros((len(z), 3), float)
+for i in range(0, len(z)):
+    front_pts[i][0] = x_front[i]
+    front_pts[i][1] = y_front[i]
+    front_pts[i][2] = z[i]
+
+    tail_pts[i][0] = x_tail[i]
+    tail_pts[i][1] = y_tail[i]
+    tail_pts[i][2] = z[i]
+
+front_crv = Curve(front_pts)
+tail_crv = Curve(tail_pts)
+
+"""
+# 前缘线
+a, b, c = front_crv.generate()
+plane.AddPart(IGES_Entity126(front_crv.p, front_crv.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
+
+# 尾缘线
+a, b, c = tail_crv.generate()
+plane.AddPart(IGES_Entity126(tail_crv.p, tail_crv.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
+
+# 前后缘采样点
+for i in range(0, len(z)):
+    plane.AddPart(IGES_Entity116(x_front[i], y_front[i], z[i]))
+    plane.AddPart(IGES_Entity116(x_tail[i], y_tail[i], z[i]))
+
+# 根梢弦线
+plane.AddPart(IGES_Entity110([x_front[0], y_front[0], z[0]], [x_tail[0], y_tail[0], z[0]]))
+plane.AddPart(IGES_Entity110([x_front[len(z) - 1], y_front[len(z) - 1], z[len(z) - 1]], [x_tail[len(z) - 1], y_tail[len(z) - 1], z[len(z) - 1]]))
+"""
+
+profile_pts = []
+profile_crv = []
+
+for i in range(0, len(z)):
+    epts = np.array([[x_front[i], y_front[i], z[i]], [x_tail[i], y_tail[i], z[i]]])
+    wp = Wing_Profile(airfoil[i], epts)
+    pts = wp.getPointList()
+    cc = Curve(pts)
+    profile_pts.append(pts)
+    profile_crv.append(cc)
+
+    """
+    # 翼型离散点
+    for k in range(0, len(pts)):
+        plane.AddPart(IGES_Entity116(pts[k][0], pts[k][1], pts[k][2]))
+    """
+
+    # 翼型NURBS曲线，默认5阶
+    a, b, c = cc.generate()
+    plane.AddPart(IGES_Entity126(cc.p, cc.n, 1, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 1.0], float)))
+
+n = len(profile_pts[0])
+m = len(z)
+
+for i in range(0, n):
+    pts = np.zeros((m, 3), float)
+    for j in range(0, m):
+        pts[j] = profile_pts[j][i]
+
+    cc = Curve(pts)
+    a, b, c = cc.generate()
+    plane.AddPart(IGES_Entity126(cc.p, cc.n, 0, 0, 1, 0, a, b, c, 0.0, 1.0, np.array([0, 0, 0], float)))
+
+plane.Generate()
--- src/ui.py
+++ src/ui.py
@@ -3,12 +3,23 @@ from PyQt5.QtGui import QIcon
 import sys
 import os
 import math
+from src.aircraft.wing import *
 
 # 翼型文件夹路径
 airfoil_dir = '../airfoil/'
 
 # 翼型列表
 airfoil_list = []
+
+z = np.array([0., 0.624029, 1.38967, 2.43503, 3.73439, 5.25574, 6.96162, 8.81003, 10.7555, 12.75, 14.7445, 16.69, 18.5384, 20.2443, 21.7656, 23.065, 24.1103, 24.876, 25.343, 25.5])
+x_front = np.array([0., 0.05, 0.3, 1.7, 4.9, 6.85, 8.45, 9.65, 10.6, 11.1, 11.7, 12.1, 12.4, 12.8, 13.2, 13.7, 14.1, 14.5, 15.2, 16.])
+y_front = np.linspace(0, 0, len(z))
+x_tail = np.array([19.7, 19.6, 19.6, 19.5, 19.3, 19, 18.3, 17.3, 16.6, 16.5, 16.8, 17, 17.45, 17.8, 18.1, 18.4, 18.55, 18.65, 18.3, 17.8])
+y_tail = np.linspace(0, 0, len(z))
+thickness = np.ones(len(z))
+airfoil = []
+for i in range(0, len(z)):
+    airfoil.append(Airfoil("../airfoil/naca0012.dat"))
 
 
 def update_airfoil_list():
@@ -18,22 +29,117 @@ def update_airfoil_list():
             airfoil_list.append(f)
 
 
-class AircraftUI(QWidget):
-    def __init__(self):
+class Section(QWidget):
+    def __init__(self, airfoil, z, xf, yf, xt, yt, thickness):
+        super(Section, self).__init__()
+
+        # Descriptions
+        self.airfoil = airfoil
+        self.z = z
+        self.xf = xf
+        self.yf = yf
+        self.xt = xt
+        self.yt = yt
+        self.thickness = thickness
+
+        # Layout
+        self.global_layout = QHBoxLayout()
+
+        # Widgets
+        self.airfoil_combobox = QComboBox()
+        self.z_le = QLineEdit()
+        self.z_le.setText(str(self.z))
+        self.xf_le = QLineEdit()
+        self.xf_le.setText(str(self.xf))
+        self.yf_le = QLineEdit()
+        self.yf_le.setText(str(self.yf))
+        self.xt_le = QLineEdit()
+        self.xt_le.setText(str(self.xt))
+        self.yt_le = QLineEdit()
+        self.yt_le.setText(str(self.yt))
+        self.thick_le = QLineEdit()
+        self.thick_le.setText(str(self.thickness))
+
+        self.airfoil_combobox.addItems(airfoil_list)
+        self.setLayout(self.global_layout)
+
+        # Init
+        self.global_layout.addWidget(self.airfoil_combobox)
+        self.global_layout.addWidget(self.z_le)
+        self.global_layout.addWidget(self.xf_le)
+        self.global_layout.addWidget(self.yf_le)
+        self.global_layout.addWidget(self.xt_le)
+        self.global_layout.addWidget(self.yt_le)
+        self.global_layout.addWidget(self.thick_le)
+
+
+class BWB_UI(QWidget):
+    def __init__(self, _airfoil_list, _z_list, _xf_list, _yf_list, _xt_list, _yt_list, _tck_list):
         QWidget.__init__(self)
 
-    def initUI(self):
+        assert len(_airfoil_list) == len(_z_list)
+        assert len(_z_list) == len(_xf_list)
+        assert len(_xf_list) == len(_yf_list)
+        assert len(_yf_list) == len(_xt_list)
+        assert len(_xt_list) == len(_yt_list)
+        assert len(_yt_list) == len(_tck_list)
+
+        # Contents
+        self.sec_cnt = len(_airfoil_list)
+        self.section = []
+        self.airfoil_list = _airfoil_list
+        self.z_list = _z_list
+        self.xf_list = _xf_list
+        self.yf_list = _yf_list
+        self.xt_list = _xt_list
+        self.yt_list = _yt_list
+        self.thickness_list = _tck_list
+
+        # Layout
+        self.global_layout = QVBoxLayout()
+        self.param_layout = QVBoxLayout()
+        self.btn_layout = QHBoxLayout()
+
+        # Buttons
+        self.btn_model = QPushButton()
+        self.btn_model.setText('生成IGES模型')
+        self.btn_model.clicked.connect(self.gen_model)
+
+        self.btn_mesh = QPushButton()
+        self.btn_mesh.setText('生成网格')
+        self.btn_mesh.clicked.connect(self.gen_mesh)
+
+        self.btn_exit = QPushButton()
+        self.btn_exit.setText('退出')
+        self.btn_exit.clicked.connect(self.close)
+
+    def init_ui(self):
         self.setWindowTitle('BWB一体化设计')
         self.setWindowIcon(QIcon('../pic/plane_icon.jpg'))
+        self.setLayout(self.global_layout)
+        self.global_layout.addLayout(self.param_layout)
+        self.global_layout.addLayout(self.btn_layout)
+
+        self.btn_layout.addWidget(self.btn_model)
+        self.btn_layout.addWidget(self.btn_mesh)
+        self.btn_layout.addWidget(self.btn_exit)
+
+        for i in range(0, self.sec_cnt):
+            cur_sec = Section(self.airfoil_list[i], self.z_list[i], self.xf_list[i], self.yf_list[i], self.xt_list[i], self.yt_list[i], self.thickness_list[i])
+            self.section.append(cur_sec)
+            self.param_layout.addWidget(cur_sec)
+
+    def gen_model(self):
+        pass
+
+    def gen_mesh(self):
+        pass
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
     update_airfoil_list()
-
-    pm = AircraftUI()
-    pm.initUI()
+    app = QApplication(sys.argv)
+    pm = BWB_UI(airfoil, z, x_front, y_front, x_tail, y_tail, thickness)
+    pm.init_ui()
     pm.show()
-
     sys.exit(app.exec_())
