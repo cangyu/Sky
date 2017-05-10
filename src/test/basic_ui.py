89e6337e668248b1fbcbf63f31e60c5b5ff14dfb
Yu Cang <yhcy1993@gmail.com>
2017-05-10 22:49:00 +0800

    to test pyqt on mac

---
+++ src/test/basic_ui.py
@@ -0,0 +1,9 @@
+import unittest
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class BasicUI_Test(unittest.TestCase):
+    def test_window(self):
+        pass
