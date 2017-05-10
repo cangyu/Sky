5d18575d86cc1a38c34ba268491788edbc672bb3
Yu Cang <yhcy1993@gmail.com>
2017-05-10 22:48:36 +0800

    mv to test unit

---
+++ src/test/plot3d.py
@@ -0,0 +1,88 @@
+import unittest
+import numpy as np
+import math
+from src.msh.plot3d import Plot3D
+
+
+def rect(X_MIN, X_MAX, Y_MIN, Y_MAX, Z_MIN, Z_MAX, U, V, W, fout=""):
+    """
+    生成一个长方体
+    :param X_MIN: 长度方向最小值 
+    :param X_MAX: 长度方向最大值
+    :param Y_MIN: 宽度方向最小值
+    :param Y_MAX: 宽度方向最大值
+    :param Z_MIN: 高度方向最小值
+    :param Z_MAX: 高度方向最大值
+    :param U: 长度方向分段数
+    :param V: 宽度方向分段数
+    :param W: 高度方向分段数
+    :param fout: 输出文件名
+    :return: None
+    """
+
+    pts = np.zeros((W + 1, V + 1, U + 1, 3))
+
+    u_list = np.linspace(X_MIN, X_MAX, U + 1)
+    v_list = np.linspace(Y_MIN, Y_MAX, V + 1)
+    w_list = np.linspace(Z_MIN, Z_MAX, W + 1)
+
+    if fout == "":
+        fout += "rect"
+        fout += "_{}_{}".format(X_MIN, X_MAX)
+        fout += "_{}_{}".format(Y_MIN, Y_MAX)
+        fout += "_{}_{}".format(Z_MIN, Z_MAX)
+        fout += "_{}_{}_{}.xyz".format(U, V, W)
+
+    for k in range(0, W + 1):
+        for j in range(0, V + 1):
+            for u in range(0, U + 1):
+                pts[k][j][i] = np.array([u_list[i], v_list[j], w_list[k]])
+
+    msh = Plot3D(U + 1, V + 1, W + 1, pts)
+    msh.output(fout)
+
+
+def sect(R_MIN, R_MAX, THETA_MIN, THETA_MAX, H_MIN, H_MAX, U, V, W, fout=""):
+    """
+    生成一个扇形柱
+    :param R_MIN: 扇形内径
+    :param R_MAX: 扇形外径
+    :param THETA_MIN: 起始角度(Degree)
+    :param THETA_MAX: 终止角度(Degree)
+    :param H_MIN: 最小高度
+    :param H_MAX: 最大高度
+    :param U: 径向分段数
+    :param V: 周向分段数
+    :param W: 高度方向分段数
+    :param fout: 输出文件名
+    :return: None
+    """
+
+    pts = np.zeros((W + 1, V + 1, U + 1, 3))
+
+    u_list = np.linspace(R_MIN, R_MAX, U + 1)
+    v_list = np.linspace(THETA_MIN, THETA_MAX, V + 1)
+    w_list = np.linspace(H_MIN, H_MAX, W + 1)
+
+    for k in range(0, W + 1):
+        for j in range(0, V + 1):
+            for i in range(0, U + 1):
+                ct = math.radians(v_list[j])
+                pts[k][j][i] = np.array([u_list[i] * math.cos(ct), u_list[i] * math.sin(ct), w_list[k]])
+
+    msh = Plot3D(U + 1, V + 1, W + 1, pts)
+    msh.output(fout)
+
+def airfoil():
+    pass
+
+
+class Plot3D_Test(unittest.TestCase):
+    def test_rect(self):
+        rect(0, 100, 0, 60, 0, 40, 60, 15, 20)
+
+    def test_cylinder(self):
+        sect(50, 100, 60, 120, 60, 15, 20)
+
+    def test_airfoil(self):
+        airfoil()
