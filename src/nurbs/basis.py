5c89bf54c3f456bded8e214418196e3931429d01
CangYu <yhcy1993@gmail.com>
2017-05-24 21:04:00 +0800

    restart nurbs

--- src/nurbs/basis.py
+++ src/nurbs/basis.py
@@ -1,266 +1,62 @@
 import numpy as np
 import math
 
-eps = 1e-6
-
 
 def equal(a: float, b: float):
-    return True if math.fabs(a - b) < eps else False
+    return True if math.fabs(a - b) < 1e-8 else False
 
 
-def FindSpan(n: int, p: int, u: float, U):
-    """
-    确定参数u所在的节点区间的下标i, s.t. u belongs to [U[i], U[i+1])
-    :param n: 最后一个控制点下标，从0开始
-    :param p: B样条基函数的次数
-    :param u: Target parameter
-    :param U: 节点序列
-    :return: u所在的节点区间下标i
-    """
+class Basis(object):
+    def __init__(self, U, p):
+        """
+        定义在非周期节点矢量上的B样条基函数
+        :param U: 节点矢量
+        :param p: 基函数次数
+        """
 
-    if u == U[n + 1]:  # Corner case: u=U[m]，将其节点区间的下标设为n
-        return n
+        self.U = np.copy(U)
 
-    low = p
-    high = n + 1
-    mid = int((high + low) / 2)
-    while u < U[mid] or u >= U[mid + 1]:
-        if u < U[mid]:
-            high = mid
+        self.p = p
+        self.m = len(self.U)
+        self.n = self.m - self.p - 1
+
+    def __call__(self, i, p, u, d=0):
+        """
+        计算基函数导数
+        :param i: 第i个B样条基函数
+        :param p: B样条基函数次数
+        :param u: 目标参数
+        :param d: 求导次数
+        :return: N(i, p, u)的d阶导数
+        """
+
+        if d < 0:
+            raise ValueError("Order of Derivative shold be non-negative!")
+
+        if d == 0:
+            if p == 0:
+                return 1.0 if self.U[i] <= u < self.U[i + 1] else 0.0
+            else:
+                ans = 0.0
+                tmp = self.U[i + p] - self.U[i]
+                if not equal(tmp, 0.0):
+                    ans += (u - self.U[i]) / tmp * self.__call__(i, p - 1, u)
+
+                tmp = self.U[i + p + 1] - self.U[i + 1]
+                if not equal(tmp, 0.0):
+                    ans += (self.U[i + p + 1] - u) / tmp * self.__call__(i + 1, p - 1, u)
+
+                return ans
         else:
-            low = mid
-        mid = (low + high) / 2
+            ans = 0.0
+            tmp = self.U[i + p] - self.U[i]
+            if not equal(tmp, 0.0):
+                ans += self.__call__(i, p - 1, u, d - 1) / tmp
 
-    return mid
+            tmp = self.U[i + p + 1] - self.U[i + 1]
+            if not equal(tmp, 0.0):
+                ans += self.__call__(i + 1, p - 1, u, d - 1) / tmp
 
+            ans *= p
 
-def BasisFuns(i: int, u: float, p: int, U, N):
-    """
-    计算在给定点u，所有p次非零B样条基函数的值:N(i, p, u)
-    :param i: 参数u所在的节点区间，s.t. u∈[U[i],U[i+1])
-    :param u: 目标参数
-    :param p: B样条基函数的次数
-    :param U: 节点序列
-    :param N: p+1个元素，因为只有N(i-p,p,u) ~ N(i,p,u)不为0
-    :return: None
-    """
-
-    left = np.zeros(p + 1, float)
-    right = np.zeros(p + 1, float)
-
-    N[0] = 1.0
-    for j in range(1, p + 1):
-        left[j] = u - U[i + 1 - j]  # left[k], right[k]仅在计算第k阶基函数时用到，所以可以逐次计算
-        right[j] = U[i + j] - u
-        saved = 0.0
-        for r in range(0, j):
-            tmp = N[r] / (right[r + 1] + left[j - r])
-            N[r] = saved + right[r + 1] * tmp
-            saved = left[j - r] * tmp
-
-        N[j] = saved
-
-
-def AllBasisFuns(i, u, p, U, N):
-    """
-    计算所有0到p次非0基函数的值
-    :param i: 参数u所在的节点区间，s.t. u∈[U[i],U[i+1])
-    :param u: 目标参数
-    :param p: 最大B样条基函数次数
-    :param U: 节点序列
-    :param N: 所有0到p次非0基函数的值,(p+1)*(p+1)个元素，N[jj][ii]存储ii次基函数第jj个非0值，ii<=p, jj<=ii
-    :return: None
-    """
-
-    for pp in range(0, p + 1):
-        '''计算次数为pp时所有非零B样条基函数的值'''
-        data = np.zeros(pp + 1)
-        BasisFuns(i, u, pp, U, data)
-
-        '''Copy result'''
-        for k in range(0, pp + 1):
-            N[k][pp] = data[k]
-
-
-def DersBasisFuns(i: int, u: float, p: int, n: int, U, ders):
-    """
-    计算非零B样条基函数及其导数
-    :param i: 参数u所在的节点区间，s.t. u∈[U[i],U[i+1])
-    :param u: 目标参数
-    :param p: B样条基函数的次数
-    :param n: 输出非零基函数及其直到n阶导数(n<=p)
-    :param U: 节点序列
-    :param ders: (n+1)*(p+1)个元素，ders[k][j] 存储N(i-p+j, p)的k阶导数
-    :return: None
-    """
-
-    left = np.zeros(p + 1)
-    right = np.zeros(p + 1)
-    ndu = np.zeros((p + 1, p + 1))  # 存储基函数与节点之差
-    a = np.zeros((2, p + 1))  # 以不断更新的方式存储最新的两行a[k][j], a[k-1][j]
-
-    '''将函数值与节点差存放到数组中'''
-    ndu[0][0] = 1.0
-    for j in range(1, p + 1):  # 逐个矩形层推进
-        left[j] = u - U[i + 1 - j]
-        right[j] = U[i + j] - u
-        saved = 0.0
-        for r in range(0, j):
-            '''下三角'''
-            ndu[j][r] = right[r + 1] + left[j - r]
-            temp = ndu[r][j - 1] / ndu[j][r]
-            '''上三角'''
-            ndu[r][j] = saved + right[r + 1] * temp
-            saved = left[j - r] * temp
-        ndu[j][j] = saved
-
-    '''载入基函数的值'''
-    for j in range(0, p + 1):
-        ders[0][j] = ndu[j][p]
-
-    '''计算导数'''
-    for r in range(0, p + 1):  # 对函数下标进行循环
-        s1 = 0
-        s2 = 1
-        a[0][0] = 1.0
-        for k in range(1, n + 1):  # 循环计算k阶导数
-            d = 0.0
-            rk = r - k
-            pk = p - k
-
-            if r >= k:
-                a[s2][0] = a[s1][0] / ndu[pk + 1][rk]
-                d = a[s2][0] * ndu[rk][pk]
-
-            j1 = 1 if rk >= -1 else -rk
-            j2 = k - 1 if r - 1 <= pk else p - r
-
-            for j in range(j1, j2 + 1):
-                a[s2][j] = (a[s1][j] - a[s1][j - 1]) / ndu[pk + 1][rk + j]
-                d += a[s2][j] * ndu[rk + j][pk]
-
-            if r <= pk:
-                a[s2][k] = -a[s1][k - 1] / ndu[pk + 1][r]
-                d += a[s2][k] * ndu[r][pk]
-
-            ders[k][r] = d
-
-            '''转换行'''
-            j = s1
-            s1 = s2
-            s2 = j
-
-    '''乘以正确的因子'''
-    r = p
-    for k in range(1, n + 1):
-        for j in range(0, p + 1):
-            ders[k][j] *= r
-        r *= (p - k)
-
-
-def OneBasisFun(p: int, m: int, U, i: int, u: float):
-    """
-    计算基函数N(i, p)在u处的值
-    :param p: B样条基函数的次数
-    :param m: 最后一个节点的下标
-    :param U: 节点序列，m+1个元素
-    :param i: 参数u所在的节点区间，s.t. u∈[U[i],U[i+1])
-    :param u: 目标参数
-    :return: 基函数N(i, p)在u处的值
-    """
-
-    N = np.zeros(p + 1)
-
-    if (i == 0 and equal(u, U[0])) or (i == m - p - 1 and equal(u, U[m])):  # 特殊情况
-        return 1.0
-
-    if u < U[i] or u >= U[i + p + 1]:  # 局部支撑
-        return 0.0
-
-    '''初始化0次基函数'''
-    for j in range(0, p + 1):
-        if u >= U[i + j] and u < U[i + j + 1]:
-            N[j] = 1.0
-
-    '''计算三角形表'''
-    for k in range(1, p + 1):
-        saved = 0.0 if equal(N[0], 0.0) else (u - U[i]) * N[0] / (U[i + k] - U[i])
-
-        for j in range(0, p - k + 1):
-            Uleft = U[i + j + 1]
-            Uright = U[i + j + k + 1]
-            if equal(N[j + 1], 0.0):
-                N[j] = saved
-                saved = 0.0
-            else:
-                temp = N[j + 1] / (Uright - Uleft)
-                N[j] = saved + (Uright - u) * temp
-                saved = (u - Uleft) * temp
-
-    return N[0]
-
-
-def DersOneBasisFun(p, m, U, i, u, n, ders):
-    """
-    计算基函数Nip的各阶导数， 先计算出0次基函数，再由低次基函数导数值递推高次基函数导数值
-    :param p: B样条基函数的次数
-    :param m: 最后一个节点的下标
-    :param U: 节点序列，m+1个元素
-    :param i: 参数u所在的节点区间，s.t. u∈[U[i],U[i+1])
-    :param u: 目标参数
-    :param n: 输出非零基函数及其直到n阶导数(n<=p)
-    :param ders: n+1个元素，ders[k]返回k阶导数
-    :return: None
-    """
-
-    N = np.zeros((p + 1, p + 1))
-    ND = np.zeros(n + 1)
-
-    if u < U[i] or u >= U[i + p + 1]:  # 局部支撑性
-        for k in range(0, n + 1):
-            ders[k] = 0.0
-        return
-
-    '''初始化0次基函数'''
-    for j in range(0, p + 1):
-        if u >= U[i + j] and u < U[i + j + 1]:
-            N[j][0] = 1.0
-
-    '''计算完整的三角形表'''
-    for k in range(1, p + 1):
-        saved = 0.0 if equal(N[0][k - 1], 0.0) else (u - U[i]) * N[0][k - 1] / (U[i + k] - U[i])
-        for j in range(0, p - k + 1):
-            Uleft = U[i + j + 1]
-            Uright = U[i + j + k + 1]
-            if equal(N[j + 1][k - 1], 0.0):
-                N[j][k] = saved
-                saved = 0.0
-            else:
-                temp = N[j + 1][k - 1] / (Uright - Uleft)
-                N[j][k] = saved + (Uright - u) * temp
-                saved = (u - Uleft) * temp
-
-    '''函数值'''
-    ders[0] = N[0][p]
-
-    '''计算导数'''
-    for k in range(1, n + 1):
-        '''载入正确的列'''
-        for j in range(0, k + 1):
-            ND[j] = N[j][p - k]
-
-        '''计算宽度为k的表'''
-        for jj in range(1, k + 1):
-            saved = 0.0 if equal(ND[0], 0.0) else ND[0] / (U[i + p - k + jj] - U[i])
-            for j in range(0, k - jj + 1):
-                Uleft = U[i + j + 1]
-                Uright = U[i + j + p + jj + 1]
-                if equal(ND[j + 1], 0.0):
-                    ND[j] = (p - k + jj) * saved
-                    saved = 0.0
-                else:
-                    temp = ND[j + 1] / (Uright - Uleft)
-                    ND[j] = (p - k + jj) * (saved - temp)
-                    saved = temp
-
-        ders[k] = ND[0]  # k阶导数
+            return ans
--- src/nurbs/curve.py
+++ src/nurbs/curve.py
@@ -1,205 +1,69 @@
 import numpy as np
-import scipy.linalg
-from scipy import interpolate
-from scipy.special import factorial
 import math
-from src.iges.iges_entity112 import IGES_Entity112
-
-
-def GetDistance(lhs, rhs):
-    tmp = 0.0
-    for j in range(0, 3):
-        tmp += math.pow(lhs[j] - rhs[j], 2)
-    return math.sqrt(tmp)
+from scipy.misc import comb
+from src.nurbs.basis import *
+from src.iges.iges_entity126 import IGES_Entity126
 
 
 class Curve(object):
-    def __init__(self, pts, p=5):
+    def __init__(self, U, Pw):
         """
-        p阶，n+1个控制点，m+1个节点,全局插值，非有理
+        NURBS曲线
+        :param U:节点矢量 
+        :param Pw: 带权控制点
         """
-        cur_shape = pts[0].shape
-        for i in range(1, len(pts)):
-            if cur_shape != pts[i].shape:
-                raise Exception("Inconsistent shape!")
 
-        self.dim = cur_shape[0]
-        self.p = p
-        self.n = len(pts) - 1
-        self.m = self.n + self.p + 1
+        self.n = len(Pw) - 1
+        self.m = len(U) - 1
+        self.p = self.m - self.n - 1
 
-        self.pts = np.zeros((self.n + 1, self.dim), float)
+        self.U = np.copy(U)
+        self.Pw = np.copy(Pw)
+
+        self.N = Basis(self.U, self.p)
+
+    def w(self, u, d=0):
+        ans = 0.0
         for i in range(0, self.n + 1):
-            for j in range(0, self.dim):
-                self.pts[i][j] = pts[i][j]
+            ans += self.N(i, self.p, u, d) * self.Pw[3]
 
-        self.param = np.zeros(self.n + 1, float)
-        self.knots = np.zeros(self.m + 1, float)
-        self.coef = np.zeros((self.n + 1, self.n + 1), float)
-        self.ctrl_pts = np.zeros((self.n + 1, self.dim), float)
-        self.weights = np.ones(self.n + 1, float)
+        return ans
 
-    def calc_param(self, method='centripetal'):
-        self.param[0] = 0.0
-        self.param[self.n] = 1.0
-
-        if method not in ['uniform', 'chord', 'centripetal']:
-            raise Exception("Invalid parameter!")
-
-        if method == 'uniform':
-            inc = 1 / self.n
-            for i in range(1, self.n):
-                self.param[i] = self.param[i - 1] + inc
-        else:
-            dist = np.zeros(self.n + 1, float)
-            for i in range(1, self.n + 1):
-                for j in range(0, self.dim):
-                    dist[i] += math.pow(self.pts[i][j] - self.pts[i - 1][j], 2)
-                dist[i] = math.sqrt(dist[i])
-
-            d = 0
-            if method == 'chord':
-                for i in range(1, self.n + 1):
-                    d += dist[i]
-            else:
-                for i in range(1, self.n + 1):
-                    dist[i] = math.sqrt(dist[i])
-                    d += dist[i]
-
-            for i in range(1, self.n):
-                self.param[i] = self.param[i - 1] + dist[i] / d
-
-    def calc_knots(self):
+    def __call__(self, u, d=0):
         """
-        取平均值方法计算节点
+        计算曲线上的点
+        :param u: 目标参数
+        :param d: 求导次数
+        :return: 曲线在u处的d阶导矢
         """
-        for i in range(0, self.p + 1):
-            self.knots[i] = 0.0
-            self.knots[self.m - i] = 1.0
 
-        acc = 0.0
-        for i in range(0, self.p):
-            acc += self.param[i]
+        Ad = np.zeros(3)
+        nipd = np.zeros(self.n + 1)
+        for i in range(0, self.n + 1):
+            nipd[i] = self.N(i, p, u, d)
+        for i in range(0, self.n + 1):
+            for j in range(0, 3):
+                Ad[j] += nipd[i] * self.Pw[j]
 
-        for j in range(1, self.n - self.p + 1):
-            acc -= self.param[j - 1]
-            acc += self.param[self.p - 1 + j]
-            self.knots[self.p + j] = acc / self.p
+        ccw = np.zero(3)
+        i = 1
+        while i <= d:
+            ccw += comb(d, i, exact=True) * self.w(u, i) * self.__call__(u, d - i)
 
-    def calc_coef(self):
-        """
-        计算系数矩阵
-        """
-        rg = np.zeros(self.n + 1, int)
-        rg[0] = self.p
-        rg[self.n] = self.n
+        wu = self.w(u)
+        if equal(wu, 0.0):
+            raise ValueError("Invalid weight settings!")
 
-        for i in range(1, self.n):
-            left = self.p
-            right = self.n
+        return (Ad - ccw) / wu
 
-            while left < right:
-                mid = int((left + right + 1) / 2)
-                if self.param[i] < self.knots[mid]:
-                    right = mid - 1
-                else:
-                    left = mid
+    def to_iges(self, planar, closed, polynomial, periodic, sp, ep, norm, form=0):
 
-            rg[i] = left
-
-        for k in range(0, self.n + 1):
-            i = rg[k]
-            u = self.param[k]
-
-            N = BasisFuns(i, u, self.p, self.knots)
-
-            for j in range(i - self.p, i + 1):
-                self.coef[k][j] = N[j - i + self.p]
-
-    def calc_ctrl_pts(self):
-        """
-        解self.dim个线性方程组反求得控制点坐标，每个方程组中有self.n+1个方程
-        """
-        Q = np.zeros((self.dim, self.n + 1), float)
-        P = np.zeros((self.dim, self.n + 1), float)
-
-        for i in range(0, self.dim):
-            for j in range(0, self.n + 1):
-                Q[i][j] = self.pts[j][i]
-
-        for i in range(0, self.dim):
-            P[i] = scipy.linalg.solve(self.coef, Q[i])
+        w = np.zeros(self.n + 1)
+        cpt = np.zeros((self.n + 1, 3))
 
         for i in range(0, self.n + 1):
-            for j in range(0, self.dim):
-                self.ctrl_pts[i][j] = P[j][i]
+            w[i] = self.Pw[i][3]
+            for j in range(0, 3):
+                cpt[i][j] = Pw[i][j] / w[i]
 
-    def generate(self, method='centripetal'):
-        self.calc_param(method)
-        self.calc_knots()
-        self.calc_coef()
-        self.calc_ctrl_pts()
-
-        # 节点矢量， 权系数， 控制点
-        return self.knots, self.weights, self.ctrl_pts
-
-
-class Spline(object):
-    def __init__(self, pts, p=3, smooth=False):
-        # Number of pts and Order
-        self.n = pts.shape[0]
-        self.order = p
-        self.f = []
-
-        # Copy parameters and coordinates
-        self.u = np.zeros(self.n, float)
-        self.x = np.zeros(self.n, float)
-        self.y = np.zeros(self.n, float)
-        self.z = np.zeros(self.n, float)
-        for i in range(0, self.n):
-            self.x[i] = pts[i][0]
-            self.y[i] = pts[i][1]
-            self.z[i] = pts[i][2]
-
-        # 用自然坐标参数化
-        for i in range(1, self.n):
-            self.u[i] = GetDistance(pts[i], pts[i - 1]) + self.u[i - 1]
-
-        # 边界条件
-        bc_x = ([(2, 0)], [(2, 0)])
-        bc_y = ([(2, 0)], [(2, 0)])
-        bc_z = ([(2, 0)], [(2, 0)])
-
-        if smooth:
-            pder_x = (self.x[1] - self.x[0]) / self.u[1]
-            pder_y = (self.y[1] - self.y[0]) / self.u[1]
-            pder_z = (self.z[1] - self.z[0]) / self.u[1]
-            bc_x = ([(1, pder_x)], [(1, pder_x)])
-            bc_y = ([(1, pder_y)], [(1, pder_y)])
-            bc_z = ([(1, pder_z)], [(1, pder_z)])
-
-        # Interpolation Function
-        fx = interpolate.make_interp_spline(self.u, self.x, k=self.order, bc_type=bc_x)
-        fy = interpolate.make_interp_spline(self.u, self.y, k=self.order, bc_type=bc_y)
-        fz = interpolate.make_interp_spline(self.u, self.z, k=self.order, bc_type=bc_z)
-        self.f = [fx, fy, fz]
-
-        # Calculate coefficient matrix
-        self.cm = np.zeros((self.n, 3, self.order + 1))
-        for k in range(0, self.n):
-            for i in range(0, 3):
-                for j in range(0, self.order + 1):
-                    self.cm[k][i][j] = float(self.f[i](self.u[k], j) / factorial(j))
-
-    def iges(self):
-        assert self.order == 3
-        return IGES_Entity112(self.u, self.cm)
-
-    def x_rep(self):
-        return lambda u: self.f[0](u * self.u[-1])
-
-    def y_rep(self):
-        return lambda u: self.f[1](u * self.u[-1])
-
-    def z_rep(self):
-        return lambda u: self.f[2](u * self.u[-1])
+        return IGES_Entity126(self.p, self.n, planar, closed, polynomial, periodic, self.U, w, cpt, sp, ep, norm, form)
