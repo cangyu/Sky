38dd2fe4cca611d9d01e917f6e5c598e4f749dbe
CangYu <yhcy1993@gmail.com>
2017-06-02 21:59:59 +0800

    laplace basically ok for c msh

--- test/c_msh_2d.py
+++ test/c_msh_2d.py
@@ -4,7 +4,7 @@ from src.nurbs.curve import GlobalInterpolatedCrv, Line
 from src.aircraft.wing import WingProfile
 from src.iges.iges_core import IGES_Model
 from src.msh.spacing import single_exponential, double_exponential
-from src.msh.elliptical import Laplace_2D
+from src.msh.elliptical import Laplace_2D, Possion_2D
 from src.msh.tfi import Linear_TFI_2D
 
 
@@ -30,13 +30,12 @@ def ellipse_arc_len(a, b):
 
 '''Design Variables'''
 airfoil = 'NACA0012'
-fn = "{}_C_Laplace.xyz".format(airfoil)
 
-N0 = 35
+N0 = 45
 A = 3
 
-N1 = 40
-N2 = 80
+N1 = 60
+N2 = 180
 A1 = 0.5
 A2 = -2.5
 A3 = 0.5
@@ -47,14 +46,25 @@ Lf = 15 * La
 R = 10 * La
 Thickness = 1.0
 
+tfi = False
+laplace = True
+possion = False
+build_model = False
+
 '''Derived Variables'''
 ending = np.array([[0, 0, 0], [La, 0, 0]])
 af = WingProfile(airfoil, ending, Thickness, 5)
-airfoil_len = af.nurbs_rep.length()
 yhu = af.pts[0][1]
 ydu = af.pts[-1][1]
-inner_len = 2 * Lt + airfoil_len
-outer_len = 2 * Lt + ellipse_arc_len(La + Lf, R) / 2
+T1 = 2 * Lt + af.nurbs_rep.length()
+T3 = 2 * Lt + ellipse_arc_len(La + Lf, R) / 2
+u1 = 1 / 3
+u2 = 2 / 3
+# u1 = Lt / T1
+# u2 = (T1 - Lt) / T1
+
+# u1 = Lt / T3
+# u2 = (T3 - Lt) / T3
 
 '''C2,C4'''
 tul = Line([La + Lt, yhu, 0], [La + Lt, R, 0])
@@ -72,17 +82,15 @@ def c4(u):
 '''C1'''
 hu = Line([La + Lt, yhu, 0], [La, yhu, 0])
 du = Line([La, ydu, 0], [La + Lt, ydu, 0])
-c1u1 = Lt / inner_len
-c1u2 = (inner_len - Lt) / inner_len
 
 
 def c1(u: float):
-    if u <= c1u1:
-        ans = hu(u / c1u1)
-    elif u <= c1u2:
-        ans = af.nurbs_rep((u - c1u1) / (c1u2 - c1u1))
+    if u <= u1:
+        ans = hu(u / u1)
+    elif u <= u2:
+        ans = af.nurbs_rep((u - u1) / (u2 - u1))
     else:
-        ans = du((u - c1u2) / (1.0 - c1u2))
+        ans = du((u - u2) / (1.0 - u2))
 
     return ans[0:2]
 
@@ -101,34 +109,34 @@ for i in range(1, N1):
 outer = np.copy(outer)
 outer_crv = GlobalInterpolatedCrv(outer, method='chord')
 
-c3u1 = Lt / outer_len
-c3u2 = (outer_len - Lt) / outer_len
 
-'''
 def c3(u: float):
-    if u <= c3u1:
-        ans = l0(u / c3u1)
-    elif u <= c3u2:
-        ans = l1((u - c3u1) / (c3u2 - c3u1))
+    if u <= u1:
+        ans = l0(u / u1)
+    elif u <= u2:
+        ans = l1((u - u1) / (u2 - u1))
     else:
-        ans = l2((u - c3u2) / (1.0 - c3u2))
+        ans = l2((u - u2) / (1.0 - u2))
 
     return ans[0:2]
+
+
 '''
-
-
 def c3(u: float):
     return outer_crv(u)[0:2]
-
+'''
 
 '''Boundary distribution'''
-pv0 = single_exponential(0.0, 1.0, N0 + 1, A)
-pv1 = single_exponential(0.0, 1.0, N0 + 1, A)
+pv = single_exponential(0.0, 1.0, N0 + 1, A)
+# pv = np.linspace(0.0, 1.0, N0 + 1)
+# pu = np.linspace(0.0, 1.0, 2 * N1 + N2 + 1)
+# pu = double_exponential(0.0, 1.0, 2 * N1 + N2 + 1, A1, A2, A3)
+
 
 pu = []
-pu1 = single_exponential(0.0, c1u1, N1 + 1, -A)
-pu2 = double_exponential(c1u1, c1u2, N2 + 1, A1, A2, A3)
-pu3 = single_exponential(c1u2, 1.0, N1 + 1, A)
+pu1 = single_exponential(0.0, u1, N1 + 1, -A)
+pu2 = double_exponential(u1, u2, N2 + 1, A1, A2, A3)
+pu3 = single_exponential(u2, 1.0, N1 + 1, A)
 for u in pu1:
     pu.append(u)
 
@@ -138,39 +146,29 @@ for i in range(1, len(pu2)):
 for i in range(1, len(pu3)):
     pu.append(pu3[i])
 
-pu0 = np.copy(pu)
+pu = np.copy(pu)
 
-pu = []
-pu1 = np.linspace(0.0, c3u1, N1 + 1)
-pu2 = np.linspace(c3u1, c3u2, N2 + 1)
-pu3 = np.linspace(c3u2, 1.0, N1 + 1)
-for u in pu1:
-    pu.append(u)
+if tfi:
+    grid = Linear_TFI_2D(c1, c2, c3, c4)
+    grid.calc_msh(pu, pv)
+    grid.write_plot3d(airfoil + "_TFI.xyz")
 
-for i in range(1, len(pu2)):
-    pu.append(pu2[i])
+if laplace:
+    grid = Laplace_2D(c1, c2, c3, c4, pu, pv)
+    grid.calc_grid()
+    grid.write_plot3d(airfoil + "_Laplace.xyz")
 
-for i in range(1, len(pu3)):
-    pu.append(pu3[i])
+if possion:
+    grid = Possion_2D(c1, c2, c3, c4, pu, pv)
+    grid.calc_grid(100)
+    grid.write_plot3d(airfoil + "_Possion.xyz")
 
-pu1 = np.copy(pu)
-
-'''
-grid = Laplace_2D(c1, c2, c3, c4, pu0, pu1, pv0, pv1)
-grid.calc_grid()
-grid.write_plot3d(fn)
-'''
-
-grid = Linear_TFI_2D(c1, c2, c3, c4)
-grid.calc_msh(pu0, pu1, pv0, pv1)
-grid.write_plot3d(fn)
-
-'''Model'''
-model = IGES_Model('frame.igs')
-model.add_entity(af.nurbs_rep.to_iges(1, 0, [0, 0, 1]))
-model.add_entity(hu.to_iges())
-model.add_entity(du.to_iges())
-model.add_entity(outer_crv.to_iges(1, 0, [0, 0, 1]))
-model.add_entity(tul.to_iges())
-model.add_entity(tdl.to_iges())
-model.write()
+if build_model:
+    model = IGES_Model('frame.igs')
+    model.add_entity(af.nurbs_rep.to_iges(1, 0, [0, 0, 1]))
+    model.add_entity(hu.to_iges())
+    model.add_entity(du.to_iges())
+    model.add_entity(outer_crv.to_iges(1, 0, [0, 0, 1]))
+    model.add_entity(tul.to_iges())
+    model.add_entity(tdl.to_iges())
+    model.write()
