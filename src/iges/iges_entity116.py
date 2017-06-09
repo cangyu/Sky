4b339335845e1018a72c65d649d3d8328ba26236
CangYu <yhcy1993@gmail.com>
2017-06-09 23:00:43 +0800

    reformat

--- src/iges/iges_entity116.py
+++ src/iges/iges_entity116.py
@@ -1,11 +1,12 @@
 from src.iges.iges_core import *
 
-class IGES_Entity116(IGES_Entity):
-    '''
-    Point Entity
-    '''
 
+class IGES_Entity116(IGES_Entity):
     def __init__(self, _x, _y, _z, _ptr=0):
+        """
+        Point Entity
+        """
+
         super(IGES_Entity116, self).__init__(116)
 
         self.X = float(_x)
@@ -14,8 +15,8 @@ class IGES_Entity116(IGES_Entity):
         self.PTR = int(_ptr)
 
     def BuildParam(self):
-        param=""
-        param+="{},".format(self.directory.Entity_Type_Number)
-        param+="{},{},{},{};".format(self.X,self.Y,self.Z,self.PTR)
+        param = ""
+        param += "{},".format(self.directory.Entity_Type_Number)
+        param += "{},{},{},{};".format(self.X, self.Y, self.Z, self.PTR)
 
         return self.ConvertRawToFormatted(param)
