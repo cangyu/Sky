f574ed36e21c5f4b509d49622e371892ad77a320
CangYu <yhcy1993@gmail.com>
2017-06-09 23:01:23 +0800

    basic framework to represent a mesh

---
+++ src/msh/basic_elem.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Point(object):
+    def __init__(self):
+        pass
+
+
+class Edge(object):
+    def __init__(self):
+        pass
+
+
+class Face(object):
+    def __init__(self):
+        pass
+
+
+class Cell(object):
+    def __init__(self):
+        pass
+
+
+class Mesh(object):
+    def __init__(self):
+        pass
