9805b0d91c43458ea73cb53a59fe932a2418f9d8
Yu Cang <yhcy1993@gmail.com>
2017-06-11 21:29:18 +0800

    test frame

--- src/msh/basic_elem.py
+++ src/msh/basic_elem.py
@@ -2,25 +2,32 @@ import numpy as np
 
 
 class Point(object):
-    def __init__(self):
-        pass
-
-
-class Edge(object):
-    def __init__(self):
-        pass
+    def __init__(self, index, *args):
+        self.index = index
+        self.dim = len(args)
+        self.x = np.empty(self.dim, float)
+        for i in range(self.dim):
+            self.x[i] = args[i]
 
 
 class Face(object):
-    def __init__(self):
-        pass
+    def __init__(self, index, *args):
+        self.index = index
+        self.pts = []
+        for pnt in args:
+            self.pts.append(pnt)
 
 
 class Cell(object):
-    def __init__(self):
-        pass
+    def __init__(self, index, *args):
+        self.index = index
+        self.face_list = []
+        for face in args:
+            self.face_list.append(face)
 
 
 class Mesh(object):
     def __init__(self):
-        pass
+        self.pnt_list = []
+        self.face_list = []
+        self.cell_list = []
---
+++ test/fluent_msh_2d.py
@@ -0,0 +1,23 @@
+import numpy as np
+from src.msh.tfi import Linear_TFI_2D
+
+
+def rectangular(L: float, W: float):
+    return Linear_TFI_2D(lambda u: np.array([L * u, 0, 0]),  # c1
+                         lambda u: np.array([L * u, W, 0]),  # c2
+                         lambda v: np.array([0, W * v, 0]),  # c3
+                         lambda v: np.array([L, W * v, 0]))  # c4
+
+
+U = 8
+V = 4
+L = 5
+W = 2
+
+if __name__ == '__main__':
+    u_list = np.linspace(0, 1.0, U + 1)
+    v_list = np.linspace(0, 1.0, V + 1)
+    ppu, ppv = np.meshgrid(u_list, v_list, indexing='ij')
+    msh = rectangular(L, W)
+    msh.calc_grid(ppu, ppv)
+    print(msh.grid)
