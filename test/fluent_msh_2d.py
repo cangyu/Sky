b5dff0473190ffaffc263baa4b4c2b97b2206ab8
CangYu <yhcy1993@gmail.com>
2017-06-13 19:33:31 +0800

    add test case

--- test/fluent_msh_2d.py
+++ test/fluent_msh_2d.py
@@ -1,25 +1,34 @@
 import numpy as np
 from src.msh.tfi import Linear_TFI_2D
-from src.msh.fluent import XF_MSH
+from src.msh.fluent import XF_MSH, BCType
 
 
-def rectangular(L: float, W: float):
-    return Linear_TFI_2D(lambda u: np.array([L * u, 0, 0]),
-                         lambda v: np.array([0, W * v, 0]),
-                         lambda u: np.array([L * u, W, 0]),
-                         lambda v: np.array([L, W * v, 0]))
-
-
-U = 30
-V = 10
-L = 3
-W = 1
-
-if __name__ == '__main__':
+def rectangular(U: int, V: int, L: float, W: float):
     u_list = np.linspace(0, 1.0, U + 1)
     v_list = np.linspace(0, 1.0, V + 1)
     ppu, ppv = np.meshgrid(u_list, v_list, indexing='ij')
-    msh = rectangular(L, W)
+    msh = Linear_TFI_2D(lambda u: np.array([L * u, 0, 0]),
+                        lambda v: np.array([0, W * v, 0]),
+                        lambda u: np.array([L * u, W, 0]),
+                        lambda v: np.array([L, W * v, 0]))
     msh.calc_grid(ppu, ppv)
-    fmsh = XF_MSH.from_str2d(msh.get_grid())
-    fmsh.save('test.msh')
+    fluent_grid = XF_MSH.from_str2d(msh.get_grid())
+    fluent_grid.save('rect_{}_{}.msh'.format(U, V))
+
+
+def curve_rect(U: int, V: int, L: float, H1: float, H2: float, H3: float):
+    u_list = np.linspace(0, 1.0, U + 1)
+    v_list = np.linspace(0, 1.0, V + 1)
+    ppu, ppv = np.meshgrid(u_list, v_list, indexing='ij')
+    msh = Linear_TFI_2D(lambda u: np.array([u * L, 4 * H3 * u * (1 - u), 0]),
+                        lambda v: np.array([0, v * H1, 0]),
+                        lambda u: np.array([u * L, (H1 * (1 - u * u) + H2 * u * u), 0]),
+                        lambda v: np.array([L, v * H2, 0]))
+    msh.calc_grid(ppu, ppv)
+    fluent_grid = XF_MSH.from_str2d(msh.get_grid())
+    fluent_grid.save('crv_rect_{}_{}.msh'.format(U, V))
+
+
+if __name__ == '__main__':
+    # rectangular(30, 10, 3, 1)
+    curve_rect(50, 25, 100, 40, 60, 10)
