5894b737cbd15c7eae5eee256106c727060749f6
CangYu <yhcy1993@gmail.com>
2017-06-19 09:58:24 +0800

    reverse tested

---
+++ test/nurbs/crv_reverse.py
@@ -0,0 +1,18 @@
+from src.nurbs.curve import Arc
+from src.iges.iges_core import IGES_Model
+
+model = IGES_Model('original.igs')
+arc1 = Arc(200, 180)
+arc2 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+model.add_entity(arc1.to_iges(1, 0, [0, 0, 1]))
+model.add_entity(arc2.to_iges(1, 0, [0, 1, 0]))
+model.write()
+
+model = IGES_Model('reversed.igs')
+arc1 = Arc(200, 180)
+arc1.reverse()
+arc2 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+arc2.reverse()
+model.add_entity(arc1.to_iges(1, 0, [0, 0, 1]))
+model.add_entity(arc2.to_iges(1, 0, [0, 1, 0]))
+model.write()
