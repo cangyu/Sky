cf54eb02b392ba7c3fb80cc3475e5638c26bf847
CangYu <yhcy1993@gmail.com>
2017-07-03 09:39:55 +0800

    import optimized, add view

--- test/nurbs/crv_pan.py
+++ test/nurbs/crv_pan.py
@@ -1,7 +1,12 @@
 from src.nurbs.curve import Arc
 from src.iges.iges_core import IGES_Model
 
-model = IGES_Model('test.igs')
+try:
+    from src.com.catia import view
+except ImportError:
+    print('Win32 required for CATIA usage!')
+
+auto_view = True
 
 dir1 = [0, 0, 300]
 dir2 = [200, 0, 0]
@@ -13,9 +18,12 @@ arc3.pan(dir1)
 arc4 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
 arc4.pan(dir2)
 
+model = IGES_Model('test.igs')
 model.add_entity(arc1.to_iges(1, 0, [0, 0, 1]))
 model.add_entity(arc2.to_iges(1, 0, [0, 1, 0]))
 model.add_entity(arc3.to_iges(1, 0, [0, 0, 1]))
 model.add_entity(arc4.to_iges(1, 0, [0, 1, 0]))
-
 model.write()
+
+if auto_view:
+    view('test.igs')
