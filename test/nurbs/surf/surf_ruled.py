c103d86e0c78a4959f8cbe594f65b9c6005332d3
CangYu <yhcy1993@gmail.com>
2017-07-03 15:04:20 +0800

    ruled surf ok

---
+++ test/nurbs/surf/surf_ruled.py
@@ -0,0 +1,22 @@
+from src.aircraft.wing import WingProfile
+from src.nurbs.surface import RuledSurf
+from src.iges.iges_core import IGES_Model
+
+try:
+    from src.com.catia import view
+except ImportError:
+    print('Win32 required for CATIA usage!')
+
+auto_view = True
+
+foil1 = WingProfile('M6', [[0, 0, 0], [10, 0, 0]], p=4).nurbs_rep
+foil2 = WingProfile('NACA0012', [[0, 0, 80], [15, 0, 80]], p=3).nurbs_rep
+surf = RuledSurf(foil1, foil2)
+
+fn = 'ruled.igs'
+model_file = IGES_Model(fn)
+model_file.add_entity(surf.to_iges(0, 0, 0, 0))
+model_file.write()
+
+if auto_view:
+    view(fn)
