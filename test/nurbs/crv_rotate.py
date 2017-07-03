8e473d76cbf98a50aa110580562bc11304f10e55
CangYu <yhcy1993@gmail.com>
2017-07-03 10:32:05 +0800

    add cases

---
+++ test/nurbs/crv_rotate.py
@@ -0,0 +1,39 @@
+import unittest
+from src.nurbs.curve import Arc
+from src.iges.iges_core import IGES_Model
+
+try:
+    from src.com.catia import view
+except ImportError:
+    print('Win32 required for CATIA usage!')
+
+auto_view = True
+
+
+class RotateTest(unittest.TestCase):
+    @staticmethod
+    def test():
+        arc1 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+        arc2 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+        arc2.rotate([0, 0, 0], [1, 1, 1], 30)
+        arc3 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+        arc3.rotate([0, 0, 0], [0, 0, 1], 45)
+        arc4 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+        arc4.rotate([0, 0, 0], [0, 0, 1], 90)
+        arc5 = Arc.from_2pnt([0, 0, 500], [0, 0, 100], 180, [0, 1, 0])
+        arc5.rotate([0, 0, 0], [0, 0, 1], 120)
+
+        model = IGES_Model('test.igs')
+        model.add_entity(arc1.to_iges())
+        model.add_entity(arc2.to_iges())
+        model.add_entity(arc3.to_iges())
+        model.add_entity(arc4.to_iges())
+        model.add_entity(arc5.to_iges())
+        model.write()
+
+        if auto_view:
+            view('test.igs')
+
+
+if __name__ == '__main__':
+    unittest.main()
