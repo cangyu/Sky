9b11acfd53f8512a3a528a55e9b3e38c567c1f16
CangYu <yhcy1993@gmail.com>
2017-07-03 14:12:00 +0800

    add view

--- test/nurbs/surf/surf_bilinear.py
+++ test/nurbs/surf/surf_bilinear.py
@@ -3,6 +3,13 @@ import numpy as np
 from src.nurbs.surface import BilinearSurf
 from src.iges.iges_core import IGES_Model
 
+try:
+    from src.com.catia import view
+except ImportError:
+    print('Win32 required for CATIA usage!')
+
+auto_view = True
+
 L = 10
 P1 = np.array([[[0, 0, 0], [0, L, L]],
                [[L, 0, L], [L, L, 0]]], float)
@@ -14,8 +21,9 @@ P2 = np.array([[[0, 0, L], [0, L, 0]],
 def build_bilinear_surf(P, fn):
     bsf = BilinearSurf(P)
     model_file = IGES_Model(fn)
-    model_file.add_entity(bsf.to_iges())
+    model_file.add_entity(bsf.to_iges(0, 0, 0, 0))
     model_file.write()
+    view(fn)
 
 
 class BasicSurfTest(unittest.TestCase):
@@ -23,3 +31,6 @@ class BasicSurfTest(unittest.TestCase):
     def test_bilinear():
         build_bilinear_surf(P1, 'surf1.igs')
         build_bilinear_surf(P2, 'surf2.igs')
+
+if __name__ == '__main__':
+    unittest.main()
