06e0789309873b8d2b356420703132de91595a1d
Yu Cang <yhcy1993@gmail.com>
2017-07-05 14:31:34 +0800

    pan tested

--- test/nurbs/surf/surf_pan.py
+++ test/nurbs/surf/surf_pan.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from src.nurbs.surface import BilinearSurf
+from src.nurbs.surface import BilinearSurf, ClampedNURBSSurf
 from src.iges.iges_core import IGES_Model
 from copy import deepcopy
 
@@ -9,7 +9,7 @@ try:
 except ImportError:
     print('Win32 required for CATIA usage!')
 
-auto_view = False
+auto_view = True
 
 L = 10
 P1 = np.array([[[0, 0, 0], [0, L, L]],
@@ -19,23 +19,32 @@ P2 = np.array([[[0, 0, L], [0, L, 0]],
                [[L, 0, L], [L, L, 0]]], float)
 
 
-def build_bilinear_surf(P, fn):
-    bsf0 = BilinearSurf(P)
-    bsf1 = deepcopy(bsf0)
-    bsf1.pan([5, 5, 5])
+def surf_pan_cmp(s, dv, fn):
+    """
+    构造对比
+    :param s: 原始曲面
+    :type s: ClampedNURBSSurf
+    :param dv: 偏移向量
+    :param fn: 文件名
+    :return: None
+    """
+
+    ss = deepcopy(s)
+    ss.pan(dv)
+
     model_file = IGES_Model(fn)
-    model_file.add_entity(bsf0.to_iges())
-    model_file.add_entity(bsf1.to_iges())
+    model_file.add_entity(s.to_iges())
+    model_file.add_entity(ss.to_iges())
     model_file.write()
     if auto_view:
         view(fn)
 
 
-class BasicSurfTest(unittest.TestCase):
+class PanTest(unittest.TestCase):
     @staticmethod
-    def test_bilinear():
-        build_bilinear_surf(P1, 'surf1.igs')
-        build_bilinear_surf(P2, 'surf2.igs')
+    def test():
+        surf_pan_cmp(BilinearSurf(P1), (0, 0, 10), 'surf1.igs')
+        surf_pan_cmp(BilinearSurf(P2), (0, 0, 10), 'surf2.igs')
 
 
 if __name__ == '__main__':
