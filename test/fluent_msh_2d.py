36bf7ec251d4e0d5afb6b4a749d4789636a8f074
Yu Cang <yhcy1993@gmail.com>
2017-07-06 21:39:47 +0800

    vtk is such a pain...

--- test/fluent_msh_2d.py
+++ test/fluent_msh_2d.py
@@ -43,12 +43,16 @@ def airfoil(U, V, foil, ends, thk, order, arc_start, arc_end, theta, nv):
     ppu, ppv = np.meshgrid(u_list, v_list, indexing='ij')
     msh = Linear_TFI_2D(u0, v0, u1, v1)
     msh.calc_grid(ppu, ppv)
-    fluent_grid = XF_MSH.from_str2d(msh.get_grid())
+    fluent_grid = XF_MSH.from_str2d(msh.get_grid(), bc=(BCType.Wall, BCType.Outflow, BCType.VelocityInlet, BCType.Outflow))
     fn = '{}_{}_{}.msh'.format(foil, U, V)
     fluent_grid.save(fn)
 
 
+def airfoil_c(U, V, foil, ends, thk, order, arc_start, arc_end, theta, nv):
+    pass
+
+
 if __name__ == '__main__':
-    rectangular(30, 10, 3, 1)
-    curve_rect(50, 25, 100, 40, 60, 10)
+    # rectangular(30, 10, 3, 1)
+    # curve_rect(50, 25, 100, 40, 60, 10)
     airfoil(130, 45, 'M6', [[0, 0, 5], [20, 0, 5]], 1.6, 5, [25, 60, 5], [25, -60, 5], 180, [0, 0, 1])
