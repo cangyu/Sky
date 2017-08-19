1cab03cc7baabbbf366552a91754a4fb645b3133
CangYu <yhcy1993@gmail.com>
2017-08-19 23:17:03 +0800

    fix typos, add detailed docstrings, refine linear stretch

--- src/msh/spacing.py
+++ src/msh/spacing.py
@@ -3,21 +3,46 @@ from scipy.optimize import newton
 
 
 def linear_expand(seq, begin, end):
-    r = np.copy(seq)
-    delta = end - begin
-    for i in range(len(seq)):
-        r[i] *= delta
-        r[i] += begin
-    return r
+    """
+    将序列线性缩放到给定的起始值
+    :param seq: 待缩放序列
+    :param begin: 缩放后的起点
+    :type begin: float
+    :param end: 缩放后的终点
+    :type end:float
+    :return: 缩放后的序列
+    """
+
+    rmin = min(seq)
+    rmax = max(seq)
+    dlt = end - begin
+    rdlt = rmax - rmin
+    if rdlt == 0:
+        return np.full_like(seq, begin)
+    else:
+        return np.copy(list(map(lambda u: (u - rmin) / rdlt * dlt + begin, seq)))
+
+
+def uniform(N):
+    """
+    均匀分布
+    :param N: 节点数量
+    :type N: int
+    :return: [0,1]之间均匀分布序列
+    """
+
+    return np.linspace(0, 1, N)
 
 
 def single_exponential(N, A):
     """
     单指数分布
     :param N: 节点数量
+    :type N: int
     :param A: >0 : 节点向起始位置聚集
               <0 : 节点向终止位置聚集
-    :return: [0,1]之间新的节点分布
+    :type A: float
+    :return: [0,1]之间新的单指数分布
     """
 
     if np.fabs(A) < 1e-12:
@@ -37,11 +62,15 @@ def double_exponential(N, A1, A2, A3):
     """
     双指数分布，N个节点，曲线经过(A3, A1)
     :param N: 节点数量
+    :type N: int
     :param A1: 控制点纵坐标
+    :type A1: float
     :param A2: >0 : 向起始点处聚集
                <0 : 向终止点处聚集
+    :type A2: float
     :param A3: 控制点横坐标
-    :return: [0,1]之间新的节点分布
+    :type A3: float
+    :return: [0,1]之间的双指数分布
     """
 
     p = (1 - A1) * A3 * (np.exp(A2) - 1) / ((1 - A3) * A1 * A2 * np.exp(A2))
@@ -77,8 +106,10 @@ def hyperbolic_tangent(N, B):
     """
     双曲正切分布
     :param N: 节点数量
+    :type N: int
     :param B: 控制参数
-    :return: [0,1]之间新的节点分布
+    :type B: float
+    :return: [0,1]之间的双曲正切分布
     """
 
     rho = np.linspace(0.0, 1.0, N)
@@ -95,8 +126,10 @@ def hyperbolic_sine(N, C):
     """
     双曲正弦分布
     :param N: 节点数量
+    :type N: int
     :param C: 控制参数
-    :return: [0,1]之间新的节点分布
+    :type C: float
+    :return: [0,1]之间的双曲正弦分布
     """
 
     rho = np.linspace(0.0, 1.0, N)
--- test/msh/spacing.py
+++ test/msh/spacing.py
@@ -40,15 +40,15 @@ def plot_hyperbolic_sine(N, C):
     plt.plot(np.linspace(0.0, 1.0, N), r)
     plt.xlabel('u')
     plt.ylabel('f(u)')
-    plt.title('Hyperbolic Tangent\nC={}'.format(C))
+    plt.title('Hyperbolic Sine\nC={}'.format(C))
     plt.show()
 
 
 class SpacingTest(unittest.TestCase):
     @classmethod
     def test(cls):
-        plot_single(101, 2)
+        plot_single(101, 6)
         plot_double(401, 0.5, -1.5, 0.5)  # A2取负值使得中间较密
         plot_double(401, 0.5, 1.5, 0.5)  # A2取正值使得中间稀疏，两边密集
-        plot_hyperbolic_tangent(201, 2)
+        plot_hyperbolic_tangent(201, 3)
         plot_hyperbolic_sine(201, 2.5)
