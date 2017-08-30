5230dfa497a75903f37aae495329af453da6fcd6
CangYu <yhcy1993@gmail.com>
2017-08-30 14:57:23 +0800

    fragmentary tool scripts

---
+++ src/misc/LD_convert.py
@@ -0,0 +1,12 @@
+import sys
+from math import sin, cos, radians
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage \'python LD_convert.py r theta\'")
+        exit(-1)
+
+    r = float(sys.argv[1])
+    theta = radians(float(sys.argv[2]))
+    rr = (r*cos(theta)-sin(theta))/(r*sin(theta)+cos(theta))
+    print(rr)
