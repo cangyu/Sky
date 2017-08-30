f0d6c3179bedf9754cf97c1757787b0a8b83287a
CangYu <yhcy1993@gmail.com>
2017-08-30 15:13:03 +0800

    profile airfoil optimized

--- test/aircraft/BlendedWingBody.py
+++ test/aircraft/BlendedWingBody.py
@@ -12,29 +12,31 @@ except ImportError:
 else:
     auto_view = True
 
-c_root = 10
-c_mid = 6
-c_tip = 1.1
-b_mid = 4.5
-b_tip = 13
-alpha_mid = 35
-alpha_tip = 30
-frm = BWBFrame(c_root, c_mid, c_tip, b_mid, b_tip, alpha_mid, alpha_tip)
-print(frm)
-frm.show()
+c_root = 14
+c_mid = 6.8
+c_tip = 1.3
+b_mid = 8.1
+b_tip = 21
+alpha_mid = 32
+alpha_tip = 28
 
 inner_sec_num = 6
-outer_sec_num = 8
+outer_sec_num = 9
 u_mid = b_mid / b_tip
 u_dist = chebshev_dist_multi([0, u_mid, 1], [inner_sec_num, outer_sec_num])
 sec_num = len(u_dist)
 
-foil = ['NACA0012', 'NACA0012', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6', 'M6']
+frm = BWBFrame(c_root, c_mid, c_tip, b_mid, b_tip, alpha_mid, alpha_tip)
+print(frm)
+frm.show(u_dist)
+
+foil = ['SC20414', 'SC20414', 'SC20612', 'SC20712', 'SC20710', 'SC20710',
+        'SC20710', 'SC21010', 'SC21010', 'SC21006', 'SC20706', 'SC20706', 'SC20606', 'SC20406']
 z_offset = list(map(frm.z, u_dist))
-length = list(map(lambda u: frm.x_tail(u) - frm.x_front(u), u_dist))
+length = list(map(lambda u: frm.chord_len(u), u_dist))
 sweep_back = list(map(lambda u: math.degrees(math.atan2(frm.x_front(u), frm.z(u))), u_dist))
-twist = np.array([3, 2.5, 2.2, 2, 2, 1.8, 1.6, 1.1, 1, 1, 0.5, 0.2, 0])
-dihedral = np.array([0, 1, 1.1, 1.2, 2, 2, 2, 2, 2, 2, 2.5, 3, 3])
+twist = np.zeros(sec_num)
+dihedral = np.zeros(sec_num)
 twist_pos = np.full(sec_num, 0.25)
 y_ref = np.zeros(sec_num)
 thickness_factor = np.ones(sec_num)
