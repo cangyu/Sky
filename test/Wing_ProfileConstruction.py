7173fb2a2fe569a96621e5502fc92b80c4660c29
CangYu <yhcy1993@gmail.com>
2018-01-05 18:13:54 +0800

    profile rotation revised, test ok

---
+++ test/Wing_ProfileConstruction.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from wing import WingProfile, Airfoil
+
+if __name__ == '__main__':
+    af = Airfoil('NACA0012')
+    ending = np.array([(15, 10, 0), (0, 0, 0)])
+    wp = WingProfile(af, ending)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    wp.plot(ax)
+    plt.show()
