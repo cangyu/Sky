76f5acdd3929d8dac8b5aa4e562c7abac01c637c
CangYu <yhcy1993@gmail.com>
2018-01-05 15:44:48 +0800

    separate from unittest, not convenient in pycharm

---
+++ test/Airfoil_2DGridGeneration.py
@@ -0,0 +1,17 @@
+from wing import Airfoil
+
+if __name__ == '__main__':
+    # airfoil, A, B, C, N0, N1, N2, N3
+    data = [('SC(2)-0406', 30, 20, 50, 90, 60, 80, 3, 'none'),
+            ('RAE2822', 30, 20, 50, 90, 60, 80, 3, 'none'),
+            ('SC(2)-0406', 30, 20, 50, 90, 60, 80, 3, 'laplace'),
+            ('RAE2822', 30, 20, 50, 90, 60, 80, 3, 'laplace'),
+            ('NLF(1)-0414F', 30, 20, 50, 91, 61, 80, 3, 'thomas-middlecoff'),
+            ('RAE2822', 30, 20, 50, 90, 60, 80, 3, 'thomas-middlecoff')]
+
+    for k in range(len(data)):
+        fn, la, lb, lc, n0, n1, n2, n3, smt = data[k]
+        foil = Airfoil(fn)
+        bunch = foil.gen_grid(la, lb, lc, n0, n1, n2, n3, leading_smooth=smt)
+        p3d = bunch[1]
+        p3d.save(fn + '_flowfield_grid-smooth={}.xyz'.format(smt))
---
+++ test/Airfoil_PntRefine.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+from wing import Airfoil
+from grid import uniform
+
+if __name__ == '__main__':
+    airfoil = 'NACA64(3)-218'
+    refined_num = 161
+    foil_compare_fig = plt.figure()
+    original_ax = foil_compare_fig.add_subplot(211)
+    test_foil = Airfoil(airfoil)
+    test_foil.plot(original_ax)
+    original_ax.set_title(airfoil + ' original')
+    new_sp = uniform(refined_num)
+    test_foil.refine(new_sp)
+    current_ax = foil_compare_fig.add_subplot(212)
+    test_foil.plot(current_ax)
+    current_ax.set_title(airfoil + ' refined')
+    test_foil.save('{}_{}.dat'.format(airfoil, refined_num))
+    foil_compare_fig.savefig('test_airfoil_refine.png', dpi=600)
