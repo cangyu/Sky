49d6f2f889d0329c60711624290c424994e9babc
CangYu <yhcy1993@gmail.com>
2018-02-01 00:35:27 +0800

    new planform basically ok

--- example/CleanWing.py
+++ example/CleanWing.py
@@ -1,77 +1,55 @@
-from win32com import client
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-from planform import HWBInnerStraightPlanform
-from load_dist import EllipticLiftDist, LinearLiftDist, UniformLiftDist
-from profile import ProfileList, calc_profile_cl
-from airfoil import Airfoil, airfoil_interp
+from planform import HWBNoseBluntPlanform
+from load_dist import EllipticLiftDist, LinearLiftDist, UniformLiftDist, calc_global_cl
+from profile import ProfileList, calc_profile_cl, pic_profile_gamma_cl
+from airfoil import Airfoil, airfoil_interp, find_alpha
 from spacing import uniform, chebshev_dist_multi
 from iges import IGES_Model
 
-# catia = client.Dispatch('CATIA.Application')
-# catia.Visible = True
-#
-# model_doc = catia.Documents
-# model_part_doc = model_doc.Add('Part')
-# model_part = model_part_doc.Part
-# model_bodies = model_part.HybridBodies
-# model_body = model_bodies.Add()
-# model_body.Name = 'HWBWing'
-# model_sf = model_part.HybridShapeFactory
+spn2 = 21.0
+root_chord = 19.2
+middle_chord = 4.50
+tip_chord = 1.9
+leading_cpt = [(1.2, 1.9), (2.9, 3.4), (5.9, 5.4), (8.0, 6.95), (9.3, 8.15), (10.2, 9.6), (17.4, spn2)]
+trailing_cpt = [(15.15, 8.25), (15.1, 10.85)]
 
+_chord = [root_chord, middle_chord, tip_chord]
+_cpt = leading_cpt + trailing_cpt
+planform = HWBNoseBluntPlanform(_chord, _cpt)
 
-wing_spn2 = 21
-wing_root_len = 18.4
-wing_tip_len = 1.8
-wing_leading_inner_delta = (1.0, 1.55)
-wing_leading_middle_delta = (1.3, 1.75)
-wing_leading_outer_sweep = 28
-wing_trailing_inner_delta = (0.7, -2.3)
-wing_trailing_outer_spn = 13.5
-wing_trailing_outer_sweep = 12
-wing_planform = HWBInnerStraightPlanform(wing_root_len, wing_tip_len, wing_spn2,
-                                         wing_leading_inner_delta, wing_leading_middle_delta, wing_leading_outer_sweep,
-                                         wing_trailing_inner_delta, wing_trailing_outer_spn, wing_trailing_outer_sweep)
+_seg = [0.] + [leading_cpt[i][1] / spn2 for i in [0, 1, 2, 5, 6]]
+_num = [4, 3, 4, 5, 7]
+# u = chebshev_dist_multi(_seg, _num)
+u = np.array([0, 0.04, 0.09, 0.18, 0.26, 0.38, 0.51, 0.62, 0.74, 0.85, 0.93, 1])
+n = len(u)
+z = np.array([rel_pos * spn2 for rel_pos in u])
 
-wing_u = np.array([0, 0.04, 0.09, 0.18, 0.26, 0.38, 0.51, 0.62, 0.74, 0.85, 0.93, 1])
-wing_n = len(wing_u)
-wing_chord = np.array([wing_planform.chord_len(u) for u in wing_u])
-wing_z = wing_u * wing_spn2
-
-payload = 135
-w = payload * 1000 * 9.8
+payload = 180
 rho = 0.4135
 a = 299.5
 ma = 0.8
 v = ma * a
-p_inf = 0.5 * rho * v ** 2
+re = 6e7
+area = planform.area
+cl_design = calc_global_cl(payload, area, ma, rho, a)
+print('Design Lift Coefficient: {:.3f}'.format(cl_design))
 
-wing_lift_dist = LinearLiftDist(payload, 2 * wing_spn2, rho, v)
-wing_swp_025 = np.array([wing_planform.swp_025(u) for u in wing_u])
+lift_dist = LinearLiftDist(payload, planform.span, rho, v)
+swp_025 = np.array([planform.swp_025(rel_pos) for rel_pos in u])
+cl3 = calc_profile_cl(u, lift_dist, planform)
+cl2 = np.array([1.1 * cl3[i] / math.cos(math.radians(swp_025[i])) ** 2 for i in range(n)])
 
-wing_cl3 = calc_profile_cl(wing_u, wing_lift_dist, wing_planform, p_inf)
-print(wing_cl3)
-
-wing_cl2 = np.array([1.1 * wing_cl3[i] / math.cos(math.radians(wing_swp_025[i])) ** 2 for i in range(wing_n)])
-print(wing_cl2)
-
-# wing_lift_fig = plt.figure()
-# wing_vc_ax = wing_lift_fig.add_subplot(111)
-# fig_sp_n = 1000
-# fig_x_sp = np.linspace(0, wing_spn2, fig_sp_n)
-# fig_u_sp = np.linspace(0, 1, fig_sp_n)
-# fig_vc_sp = [wing_lift_dist.velocity_circulation_at(u) for u in fig_u_sp]
-# wing_vc_ax.plot(fig_x_sp, fig_vc_sp)
-# wing_vc_ax.set_xlabel('Span-wise position')
-# wing_vc_ax.set_ylabel('Velocity Circulation/({})'.format(r'$m^2 \cdot s^{-1}$'))
-# wing_cl_ax = wing_vc_ax.twinx()
-# wing_cl_ax.plot(wing_z, wing_cl2, '.-')
-# for i in range(wing_n):
-#     wing_cl_ax.text(wing_z[i], wing_cl2[i], '{:.3f}'.format(wing_cl2[i]))
-# wing_cl_ax.set_ylabel('Lift coefficient')
-# wing_lift_fig.tight_layout()
-# wing_lift_fig.savefig('clean_wing_lift_dist.png', dpi=300)
+# fig = plt.figure()
+# vc_ax = fig.add_subplot(212)
+# cl_ax = vc_ax.twinx()
+# planform_ax = fig.add_subplot(211, sharex=vc_ax)
+# pic_profile_gamma_cl(vc_ax, cl_ax, lift_dist, planform)
+# planform.pic(planform_ax, u=u)
+# fig.tight_layout()
+# fig.set_size_inches(10.5, 20.5)
+# fig.savefig('HWB_load_and_planform.png', dpi=300)
 
 naca64a218 = Airfoil.from_local('NACA64(3)-218A')
 naca63a615 = Airfoil.from_local('NACA63(2)-615A')
@@ -84,21 +62,14 @@ interp1, interp2 = airfoil_interp(naca64a218, naca63a615, [1 / 3, 2 / 3], np.arr
 # interp1.save('interp1.dat')
 # interp2.save('interp2.dat')
 
-
-wing_foil = [naca64a218, interp1, interp2, naca63a615, sc0714, sc0712, sc0712, sc0712, sc0712, sc0712, sc0712, sc0012]
-wing_twist_ang = np.array([1.685, 0.974, 0.339, -0.358, -0.430, -0.498, -0.498, -0.498, -0.498, -0.498, -0.498, 0])
-wing_twist_ref = np.array([1.0] * wing_n)
-
-wpl = ProfileList.from_planform(wing_planform, wing_foil, wing_twist_ang, wing_twist_ref, wing_u)
+foil = [naca64a218, interp1, interp2, naca63a615, sc0714, sc0712, sc0712, sc0712, sc0712, sc0712, sc0712, sc0012]
+twist = np.array([1.685, 0.974, 0.339, -0.358, -0.430, -0.498, -0.498, -0.498, -0.498, -0.498, -0.498, 0])
+# twist = np.array([find_alpha(foil[i], re, ma, cl2[i]) for i in range(n)])
+twist_ref = np.ones(n)
+wpl = ProfileList.from_planform(planform, foil, twist, twist_ref, u)
 
 model = IGES_Model()
 pfl = wpl.generate_nurbs_crv_list()
 for c in pfl:
     model.add(c.to_iges())
 model.save('HWB.igs')
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-wing_planform.pic(ax, u=wing_u)
-fig.tight_layout()
-fig.savefig('clean_wing.png', dpi=300)
