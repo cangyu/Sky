689d2fffa98a04f95fc35c483007a9bf02cb0668
CangYu <yhcy1993@gmail.com>
2018-03-07 17:57:45 +0800

    profile revised

--- example/HybridWingBody.py
+++ example/HybridWingBody.py
@@ -9,83 +9,104 @@ from load_dist import calc_global_cl
 from load_dist import LinearLiftDist, EllipticLiftDist, HybridLiftDist
 from spacing import chebshev_dist_multi
 
+'''
+    Planform Design
+'''
 spn2 = 21.0
 root_chord = 19.2
-tip_chord = 2.0
-leading_cpt = [(0.18, 0.8), (1.97, 2.2),
+tip_chord = 1.8
+leading_cpt = [(0.3, 0.8), (1.97, 2.2),
                (5.12, 4.17), (7.14, 5.47),
-               (8.58, 6.95), (10.2, 9.6), (17.4, spn2)]
-trailing_cpt = [(17.23, 3.83), (16.1, 6.97), (16.3, 10.15)]
+               (8.5, 6.95), (10.2, 9.6), (17.4, spn2)]
+trailing_cpt = [(17.23, 3.83), (16.1, 6.97), (16.25, 10.15)]
 
 _chord = [root_chord, tip_chord]
 _cpt = leading_cpt + trailing_cpt
 planform = HWBCommonPlanform(_chord, _cpt)
 
-pf_desc = [[Airfoil.from_local('NACA63A221'), 0.00, 2],
-           [Airfoil.from_local('NACA63A320'), 3.80, 2],
-           [Airfoil.from_local('NACA63A420'), 8.05, 5],
-           [Airfoil.from_local('NACA63A518'), 12.50, 3],
-           [Airfoil.from_local('NACA63A618'), 18.24, 3],
-           [Airfoil.from_local('NACA63A615'), 23.00, 12],
-           [Airfoil.from_local('SC(2)-0712'), 42.64, 21],
-           [Airfoil.from_local('SC(2)-0712'), 90.31, 2],
-           [Airfoil.from_local('SC(2)-0612'), 93.54, 3],
-           [Airfoil.from_local('SC(2)-0412'), 97.25, 3],
-           [Airfoil.from_local('SC(2)-0012'), 100.00, 0]]
-
-for e in pf_desc:
-    e[0].show()
-
-pf_n = len(pf_desc)
-fsp = chebshev_dist_multi([0, 0.5, 1], [101, 101])
-
-pf_pos = [pf_desc[0][1]]
-foil = [pf_desc[0][0]]
-for i in range(pf_n - 1):
-    local_interp_pos = np.linspace(pf_desc[i][1], pf_desc[i + 1][1], pf_desc[i][2] + 2)
-    pf_pos += list(local_interp_pos[1:])
-
-    foil_interp_pos = np.linspace(0, 1, pf_desc[i][2] + 2)[1:-1]
-    seg = airfoil_interp(pf_desc[i][0], pf_desc[i + 1][0], foil_interp_pos, [fsp] * len(foil_interp_pos))
-    foil += seg
-    foil.append(pf_desc[i + 1][0])
-
+pf_pos = [0.00, 5.24, 10.48, 15.24, 20.00,
+          24.54, 28.89, 34.50, 40.00,
+          45.70, 55.33, 64.95, 74.57, 84.00,
+          93.61, 95.87, 97.94, 100.00]
 pf_u = np.array([0.01 * x for x in pf_pos])
 pf_n = len(pf_u)
 pf_z = np.array([x * spn2 for x in pf_u])
 
+'''
+    Show Planform and Profiles
+'''
 fig = plt.figure()
 ax = fig.add_subplot(111)
 planform.pic(ax, u=pf_u)
 fig.tight_layout()
 plt.show()
 
-twist = np.zeros(pf_n)
+'''
+    Airfoil Selection and Interpolation
+'''
+root_foil = Airfoil.from_local('NACA64A221')
+cabin_foil = Airfoil.from_local('NACA64A221')
+fusion_foil = Airfoil.from_local('NACA63A418')
+inner_foil = Airfoil.from_local('NACA63A615')
+wing_foil = Airfoil.from_local('SC(2)-0712')
+outer_foil = Airfoil.from_local('SC(2)-0612')
+tip_foil = Airfoil.from_local('SC(2)-0012')
+
+fsp = chebshev_dist_multi([0, 0.5, 1], [101, 101])
+
+foil_interp1 = airfoil_interp(root_foil, cabin_foil, 0.5, fsp)
+foil_interp2 = airfoil_interp(cabin_foil, fusion_foil, 0.5, fsp)
+foil_interp3 = airfoil_interp(fusion_foil, inner_foil, 0.5, fsp)
+foil_interp4 = airfoil_interp(inner_foil, wing_foil, [1 / 3, 2 / 3], [fsp] * 2)
+foil_interp5 = airfoil_interp(wing_foil, outer_foil, [1 / 5, 2 / 5, 3 / 5, 4 / 5], [fsp] * 4)
+foil_interp6 = airfoil_interp(outer_foil, tip_foil, [1 / 3, 2 / 3], [fsp] * 2)
+
+foil = [root_foil, foil_interp1, cabin_foil, foil_interp2, fusion_foil, foil_interp3, inner_foil]
+foil += foil_interp4
+foil.append(wing_foil)
+foil += foil_interp5
+foil.append(outer_foil)
+foil += foil_interp6
+foil.append(tip_foil)
+
+'''
+    Save all airfoils for XFoil usage
+'''
+for k, f in enumerate(foil):
+    f.save('foil{}.dat'.format(k))
+
+'''
+    Twist Design
+'''
+param = np.array([[0.2953, 0.000],  # 0 - 64A221
+                  [0.2998, 0.000],  # 1
+                  [0.3006, 0.000],  # 2 - 64A221
+                  [0.6039, 0.750],  # 3
+                  [0.8702, 1.500],  # 4 - 63A418
+                  [0.9114, 1.101],  # 5
+                  [0.9855, 0.702],  # 6 - 63A615
+                  [0.8002, 0.304],  # 7
+                  [0.8083, -.095],  # 8
+                  [0.7500, -.494],  # 9 - SC(2)-0712
+                  [0.7312, -.443],  # 10
+                  [0.7129, -.392],  # 11
+                  [0.6939, -.341],  # 12
+                  [0.6725, -.290],  # 13
+                  [0.6500, -.240],  # 14 - SC(2)-0612
+                  [0.4340, -.180],  # 15
+                  [0.2154, -.090],  # 16
+                  [0.0000, 0.000]])  # 17 - SC(2)-0012
+
+cl2 = param[:, 0]
+twist = param[:, 1]
 twist_ref = np.ones(pf_n)
 wpl = ProfileList.from_planform(planform, foil, twist, twist_ref, pf_u)
 
+'''
+    Write IGES file
+'''
 model = IGES_Model()
 pfl = wpl.generate_nurbs_crv_list()
 for c in pfl:
     model.add(c.to_iges())
 model.save('HWB.igs')
-
-# payload = 200
-# rho = 0.4135  # H=10000m
-# a = 299.5
-# ma = 0.8
-# v = ma * a
-# re = 4e7
-# area = planform.area
-# cl = calc_global_cl(payload, area, ma, rho, a)
-# print('Design Lift Coefficient: {:.4f}'.format(cl))
-#
-# linear_dist = LinearLiftDist(payload, planform.span, rho, v)
-# elliptic_dist = EllipticLiftDist(payload, planform.span, rho, v)
-# lift_dist = HybridLiftDist(payload, planform.span, rho, v)
-# lift_dist.add(linear_dist, 0.35)
-# lift_dist.add(elliptic_dist, 0.65)
-# swp_025 = np.array([planform.swp_025(rel_pos) for rel_pos in u])
-# cl3 = calc_profile_cl(u, lift_dist, planform)
-# cl2 = np.array([1.1 * cl3[i] / math.cos(math.radians(swp_025[i])) ** 2 for i in range(n)])
-# print(cl3)
